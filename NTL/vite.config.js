c03e06221185a42f60452a06328fe56c711d9e77
ashraf00963 <ashrf00963@gmail.com>
2024-05-03 12:51:15 +0200

    pl

--- NTL/vite.config.js
+++ NTL/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/NextToLast/NTL/dist/',
+  base: '/NextToLast/',
   plugins: [react()],
 })
